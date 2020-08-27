c17e64f9dd852c830a20d941184362b2cebd5024
luisricardo.castillejo <luisricardo.castillejo@bbva.com>
2020-08-27 14:27:27 -0500

    Agregando modo de prueba

--- commands/kick.js
+++ commands/kick.js
@@ -4,7 +4,8 @@ module.exports = {
   execute(msg, args) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+      const fromUser = msg.author.username;
+      msg.channel.send(`${fromUser} quiere eliminarte ${taggedUser.username}`);
     } else {
       msg.reply('Etiqueta a alguien xD!');
     }
